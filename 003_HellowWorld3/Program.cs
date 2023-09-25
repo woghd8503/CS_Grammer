4c292e9bf6b8094c38da16d4fab5258e96db049a
jaehong park <woghd8503@naver.com>
2023-09-25 11:31:07 +0900

    003_HelloWorld3

---
+++ 003_HellowWorld3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+/*----------------------------------------------------------------
+ * Name: _003_HelloWorld3
+ * DESC: 다양한 형태의 메인 함수
+----------------------------------------------------------------*/
+
+namespace _003_HelloWorld3
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //      Console.WriteLine(" Static void Main(string[] args)");
+        //}
+
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("static void Main");
+        //}
+
+        //static int Main()
+        //{
+        //    Console.WriteLine("static in Main");
+        //    return 0;
+        //}
+
+        static int Main(string[] args)
+        {
+            Console.WriteLine(  "Static int Main(string[] args)");
+            return 0;
+        }
+    }
+}
