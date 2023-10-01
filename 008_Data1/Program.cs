b749ab1a6342858f6901a9e4a0f87e58a6e4b49b
jaehong park <woghd8503@naver.com>
2023-10-02 03:05:43 +0900

    009_Data2

---
+++ 008_Data1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _008_Data1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte a = 120;
+            byte b = 255;
+            short c = 30000;
+            long d = 1000000000000000000;
+            ulong f = 10000000000000000000;
+
+            Console.WriteLine( "sbyte a: " + a);
+            Console.WriteLine( "byte a: " + b);
+            Console.WriteLine( "short c: " + c);
+            Console.WriteLine( "long d: " + d);
+            Console.WriteLine( "long f: " + f);
+        }
+    }
+}
