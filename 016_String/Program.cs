b79b90bea0201ade3e96057fa723080d8737ed6f
jaehong park <woghd8503@naver.com>
2023-10-02 23:17:20 +0900

    016_String

--- 016_String/Program.cs
+++ 016_String/Program.cs
@@ -5,8 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 
 /*---------------------------------------------------------------------------
- * Name: _015_Data8
- * DESC: Char형이 아스키 코드값을 사용 정수형 데이터와 같은 것
+ * Name: _016_String
+ * DESC: 문자열 제어
 ---------------------------------------------------------------------------*/
 namespace _016_String
 {
@@ -14,10 +14,26 @@ namespace _016_String
     {
         static void Main(string[] args)
         {
-            char a = 'A'; // 아스키 코드값: 65
-            int num = a;
+            string str1 = "abcdefg";
+            string str2 = "hijklmn";
+            string str3 = str1 + str2;
 
-            Console.WriteLine("문자: {0}, 아스키코드값: {1}, 문자로 캐스트 연산: {2}", a, num, (char)num);
+            Console.WriteLine("str1 + str2: {0}", str3);
+
+            string str4 = "zzz";
+            string str5 = "zzz";
+            Console.WriteLine("str4 == str5: {0}", (str4 == str5));
+
+            int x = 10;
+            string str6 = str5 + "   " + ":" + x;
+            Console.WriteLine( "str6: {0}", str6);
+
+            string str7 = "Hello World!";
+            string str8 = str7.Replace("Hello", "Hi");
+            Console.WriteLine("str8: {0}", str8);
+
+            Console.WriteLine("str8.ToUpper(): {0}", str8.ToUpper());
+            Console.WriteLine("str8.ToLower(): {0}", str8.ToLower());
         }
     }
 }
