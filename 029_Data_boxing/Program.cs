fbbfb474ddb181d797b4a925c579216364bec1c2
jaehong park <woghd8503@naver.com>
2023-10-23 08:30:32 +0900

    029_Data_boxing

--- 029_Data_boxing/Program.cs
+++ 029_Data_boxing/Program.cs
@@ -4,12 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*-----------------------------------------------------------------------------
+ * Name: _029_Data_boxing
+ * DESC: boxing, unboxing기초(boxing하는 과정에서 heap영역에 메모리가 할당)
+-----------------------------------------------------------------------------*/
 namespace _029_Data_boxing
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            int i = 123;
+            object o = i;   //boxing
+            int j = (int)o; //unboxing
+            i = 456;
+
+            Console.WriteLine("i: {0}, o: {1}, j: {2}", i, o, j);
+            Console.WriteLine("ReferenceEquals: {0}", Object.ReferenceEquals(i, o));
         }
     }
 }
