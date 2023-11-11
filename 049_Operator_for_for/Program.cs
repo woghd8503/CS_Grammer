4729577577383b74c1ec5e3cf2a96742ffd51ffa
jaehong park <woghd8503@naver.com>
2023-11-12 03:36:13 +0900

    049_Operator_for_for

---
+++ 049_Operator_for_for/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _049_Operator_for_for
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 2; i < 10; i++)
+            {
+                for(int j = 1; j < 10; j++)
+                {
+                    Console.WriteLine("{0} X {1} = {2}", i, j, (i *j));
+                    if (j == 9) Console.WriteLine();
+                }
+            }
+        }
+    }
+}
