cf66049ace12c027a706a0bc8b7ef235046dc166
jaehong park <woghd8503@naver.com>
2023-11-12 03:46:58 +0900

    050_Operator_while(true)

--- 050_Operator_while(true)/Program.cs
+++ 050_Operator_while(true)/Program.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*-----------------------------------------------------------------------------
+ * Name: _050_Operator_while(true)
+ * DESC: 중첩 for
+-----------------------------------------------------------------------------*/
 namespace _050_Operator_while_true_
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            while(true)
+            {
+                Console.WriteLine("while(true)"); //무한으로 실행..
+            }
         }
     }
 }
