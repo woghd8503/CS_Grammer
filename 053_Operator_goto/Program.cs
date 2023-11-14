65284785f813c87c9e02f31c9d2fda09a2dd9e7a
jaehong park <woghd8503@naver.com>
2023-11-15 08:55:10 +0900

    053_Operator_goto

---
+++ 053_Operator_goto/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*-----------------------------------------------------------------------------
+ * Name: _054_Operator_goto
+ * DESC: goto 기초
+-----------------------------------------------------------------------------*/
+namespace _053_Operator_goto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                if(i == 5)
+                {
+                    goto AA;
+                }
+
+                if(i == 7)
+                {
+                    goto BB;
+                }
+            }
+
+            AA:
+                Console.WriteLine("goto AA");
+            BB:
+                Console.WriteLine("goto BB");
+        }
+    }
+}
