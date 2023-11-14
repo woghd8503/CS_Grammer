d1ebeb441ed6a6d6c79c4a92341f75b9dc8ba61f
jaehong park <woghd8503@naver.com>
2023-11-14 09:13:31 +0900

    052_Operator_continue

---
+++ 052_Operator_continue/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*-----------------------------------------------------------------------------
+ * Name: _052_Operator_continue
+ * DESC: continue 기초
+-----------------------------------------------------------------------------*/
+namespace _052_Operator_continue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                if(i == 5)
+                {
+                    Console.WriteLine();
+                    continue;
+                }
+
+                Console.WriteLine("i: " + i);
+            }
+        }
+    }
+}
