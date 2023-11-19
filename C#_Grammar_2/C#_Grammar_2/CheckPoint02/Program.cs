98a761063b1bb65913b3ec8e452f6571d02455e7
jaehong park <woghd8503@naver.com>
2023-11-20 01:02:31 +0900

    CheckPoint02

---
+++ C#_Grammar_2/C#_Grammar_2/CheckPoint02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckPoint02
+{
+    internal class Program
+    {
+        const int DELAY_TIME = 300;
+        const string LINE = "-------------------------------------";
+        const int END_LINE = 42;
+        const int FINISH_LINE = 40;
+
+        static int runA = 0;
+        static int runB = 0;
+        static int runC = 0;
+        static int runD = 0;
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
