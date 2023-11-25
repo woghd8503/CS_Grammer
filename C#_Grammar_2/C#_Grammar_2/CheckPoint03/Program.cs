2d55d43b78c47000a6f31f70a437dd23e6d5b507
jaehong park <woghd8503@naver.com>
2023-11-25 23:41:38 +0900

    CheckPoint03

---
+++ C#_Grammar_2/C#_Grammar_2/CheckPoint03/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*-----------------------------------------------------------------------------
+ * CheckPoint03 - 배열을 활용하기
+-----------------------------------------------------------------------------*/
+namespace CheckPoint03
+{
+    internal class Program
+    {
+        const int MAP_X = 7;
+        const int MAP_Y = 22;
+        const int DELAY_TIME = 300;
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
