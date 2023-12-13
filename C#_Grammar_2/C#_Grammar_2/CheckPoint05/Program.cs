60ab0d1826dbbdf27764218c490ea04978c075bb
jaehong park <woghd8503@naver.com>
2023-12-13 09:11:22 +0900

    CheckPoint05

---
+++ C#_Grammar_2/C#_Grammar_2/CheckPoint05/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckPoint05
+{
+    class CStudent
+    {
+        private int id;
+        private int kor;
+        private int math;
+        private int eng;
+        private int total;
+
+        public int ID { get { return id; } }
+        public int KOR { get { return kor; } }
+        public int MATH { get { return math; } }
+        public int ENG { get { return eng; } }
+        public int TOTAL { get { return total; } }
+
+        public CStudent(int id, int kor, int math, int eng)
+        {
+            this.id = id;
+            this.kor = kor;
+            this.math = math;
+            this.eng = eng;
+
+            this.total = kor + math + eng;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
