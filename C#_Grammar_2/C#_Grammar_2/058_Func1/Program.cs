db6c6962e2990f3db38833e9d664e0052600a7f9
jaehong park <woghd8503@naver.com>
2023-12-27 06:54:15 +0900

    058_Func1

--- C#_Grammar_2/C#_Grammar_2/058_Func1/Program.cs
+++ C#_Grammar_2/C#_Grammar_2/058_Func1/Program.cs
@@ -10,10 +10,50 @@ using System.Threading.Tasks;
 -----------------------------------------------------------------------------*/
 namespace _058_Func1
 {
-    internal class Program
+    class Program
     {
+        static void InitTitle()
+        {
+            Console.WriteLine("짝수 & 홀수 보여주기( 0 ~ 100 )");
+        }
+        
+        static void PrintEven()
+        {
+            for(int i = 0; i <= 100; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    Console.Write(" 짝수: {0}", i);
+                }
+
+                if(i % 10 == 0 && i != 0)
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        static void PrintOdd()
+        {
+            for(int i = 0; i <= 100; i++)
+            {
+                if(i % 2 != 0)
+                {
+                    Console.Write(" 홀수: {0}", i);
+                }
+
+                if(i % 10 == 1 && i != 1)
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
+            InitTitle();
+            PrintEven();
+            PrintOdd();
         }
     }
 }
