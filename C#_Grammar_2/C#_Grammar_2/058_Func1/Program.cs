5d9f85c505773b0f620edb074c300ad9e32d62dd
jaehong park <woghd8503@naver.com>
2023-12-26 09:34:01 +0900

    058_Func1

---
+++ C#_Grammar_2/C#_Grammar_2/058_Func1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*-----------------------------------------------------------------------------
+ * Name: _058_Func1
+ * DESC: 함수 기초: 리턴형 X(void), 파라미터 X
+-----------------------------------------------------------------------------*/
+namespace _058_Func1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
