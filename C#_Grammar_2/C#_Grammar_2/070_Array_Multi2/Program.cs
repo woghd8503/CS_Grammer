fbe36d4f40b425047e164c3eefd433684da4ad32
jaehong park <woghd8503@naver.com>
2024-01-11 20:00:01 +0900

    070_Array_Multi2

---
+++ C#_Grammar_2/C#_Grammar_2/070_Array_Multi2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _070_Array_Multi2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arrNum = new int[3, 2];
+        }
+    }
+}
