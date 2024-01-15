6941a67f08537dac0d49efa68f28f8da5065cc61
jaehong park <woghd8503@naver.com>
2024-01-15 09:37:31 +0900

    070_Array_Multi2

--- C#_Grammar_2/C#_Grammar_2/070_Array_Multi2/Program.cs
+++ C#_Grammar_2/C#_Grammar_2/070_Array_Multi2/Program.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*-----------------------------------------------------------------------------
+ * Name: _070_Array_Multi2
+ * DESC: 일차원 배열 다양한 선언 및 초기화 방법
+-----------------------------------------------------------------------------*/
 namespace _070_Array_Multi2
 {
     class Program
