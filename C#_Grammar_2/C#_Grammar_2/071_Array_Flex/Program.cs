5710c87e38f52e6e37300e0270cd6956a5e37f20
jaehong park <woghd8503@naver.com>
2024-02-14 20:00:01 +0900

    071_Array_Flex

---
+++ C#_Grammar_2/C#_Grammar_2/071_Array_Flex/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*-----------------------------------------------------------------------------
+ * Name: _071_Array_Flex
+ * DESC: 가변 배열
+-----------------------------------------------------------------------------*/
+namespace _071_Array_Flex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] arrNum = new int[3][]
+            {
+                arrNum[0] = new int[2] { 0, 1 };
+            }
+        }
+    }
+}
