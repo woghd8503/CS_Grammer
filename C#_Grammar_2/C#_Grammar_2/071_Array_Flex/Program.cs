d477cd079c90c90f229f248ad57091ed6d8ba9e1
jaehong park <woghd8503@naver.com>
2024-03-02 22:51:06 +0900

    071_Arr_Flex

--- C#_Grammar_2/C#_Grammar_2/071_Array_Flex/Program.cs
+++ C#_Grammar_2/C#_Grammar_2/071_Array_Flex/Program.cs
@@ -20,6 +20,26 @@ namespace _071_Array_Flex
             arrNum[1] = new int[4] { 0, 1, 2, 3 };
             arrNum[2] = new int[] { 0, 1, 2 };
 
+            Console.WriteLine("=== arrNum ===");
+
+            foreach (int[] arr in arrNum)
+            {
+                foreach(int data in arr)
+                {
+                    Console.Write("   {0}", data);
+                }
+                Console.WriteLine();
+            }
+
+            for(int i = 0; i < arrNum.Length; i++)
+            {
+
+                for(int j = 0; j < arrNum[i].Length; j++)
+                {
+                    Console.Write("   {0}", arrNum[i][j]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
