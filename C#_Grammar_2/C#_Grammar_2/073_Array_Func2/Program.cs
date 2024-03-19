22964558443535929b0d630d5c83c51b55964b76
jaehong park <woghd8503@naver.com>
2024-03-19 09:53:10 +0900

    073_Array_Func2

---
+++ C#_Grammar_2/C#_Grammar_2/073_Array_Func2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*-----------------------------------------------------------------------------
+ * Name: _073_Array_Func1
+ * DESC: 함수 파라미터로 배열 처리(CallbyReference)
+-----------------------------------------------------------------------------*/
+
+namespace _072_Array_Func1
+{
+    class Program
+    {
+        static int[] CreateIntArray(int size)
+        {
+            int[] creArray = new int[size];
+
+            for(int i = 0; i < creArray.Length; i++)
+            {
+                creArray[i] = 0;
+            }
+
+            return creArray;
+        }
+    }
+}
