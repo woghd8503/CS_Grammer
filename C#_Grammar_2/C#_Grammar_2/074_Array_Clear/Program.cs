2f5038a61b86e98bf44b11d6fbb6326d881db3d2
jaehong park <woghd8503@naver.com>
2024-03-27 09:53:31 +0900

    074_Array_Clear

--- C#_Grammar_2/C#_Grammar_2/074_Array_Clear/Program.cs
+++ C#_Grammar_2/C#_Grammar_2/074_Array_Clear/Program.cs
@@ -12,6 +12,22 @@ namespace _074_Array_Clear
 {
     class Program
     {
+        static void Main(string[] args)
+        {
+            int[] array = new int[3];
 
+            Array.Clear(array, 0, array.Length);
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = i;
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("  {0}  ", array[i]);
+            }
+
+            Console.WriteLine("\n-----------------------------");
+        }
     }
 }
