611c3fe74b9ec569722beefb73e2f245cb0eff4f
jaehong park <woghd8503@naver.com>
2024-05-05 23:59:41 +0900

    D

--- C#_Grammar_2/C#_Grammar_2/078_Constructor/obj/Debug/net8.0/078_Constructor.AssemblyInfo.cs
+++ C#_Grammar_2/C#_Grammar_2/078_Constructor/obj/Debug/net8.0/078_Constructor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("078_Constructor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a41685974296e56912bd552105fe51d6e2d8b9f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+048853ad0e7efc13d5a61508216b171fad850797")]
 [assembly: System.Reflection.AssemblyProductAttribute("078_Constructor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("078_Constructor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
