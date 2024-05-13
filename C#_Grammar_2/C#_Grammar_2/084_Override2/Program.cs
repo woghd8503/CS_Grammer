a28e7068484fb9a0487b52444f62474c1cdfe494
jaehong park <woghd8503@naver.com>
2024-05-13 20:00:01 +0900

    082_override2

--- C#_Grammar_2/C#_Grammar_2/084_Override2/Program.cs
+++ C#_Grammar_2/C#_Grammar_2/084_Override2/Program.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*-----------------------------------------------------------------------------
+ * Name: _084_override2
+ * DESC: 오버라이딩(다형성): 실제 게임에서 사용되는 예를 보여준다.
+-----------------------------------------------------------------------------*/
 namespace _084_Override2
 {
     class Program
