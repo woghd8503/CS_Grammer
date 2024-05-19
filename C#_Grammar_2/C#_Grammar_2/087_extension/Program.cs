b2c6a9db26f825b2c129fd2604f44647b64f719f
jaehong park <woghd8503@naver.com>
2024-05-19 14:18:26 +0900

    087_extension

---
+++ C#_Grammar_2/C#_Grammar_2/087_extension/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*-----------------------------------------------------------------------------
+ * Name: _087_extension
+ * DESC: 클래스 확장 함수
+-----------------------------------------------------------------------------*/
+namespace _087_extension
+{
+    class AA
+    {
+        public void PrintAA(string str) 
+        {
+            Console.WriteLine("PrintAA {0}", str);
+        }
+    }
+
+    static class Util
+    {
+        public static void Print(this AA aa, string str)
+        {
+
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
