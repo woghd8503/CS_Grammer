1a314f464ea7213e61a2887b6b2960be9005b244
jaehong park <woghd8503@naver.com>
2024-05-20 09:23:40 +0900

    087_extension

--- C#_Grammar_2/C#_Grammar_2/087_extension/Program.cs
+++ C#_Grammar_2/C#_Grammar_2/087_extension/Program.cs
@@ -22,10 +22,26 @@ namespace _087_extension
     {
         public static void Print(this AA aa, string str)
         {
-
+            aa.PrintAA(str);
         }
+
+        public static void Sum(this int a)
+        {
+            Console.WriteLine("{0} + {1} = {0}", a, a, a + a);
+        }
+    }
+
+    class Program
+    {
         static void Main(string[] args)
         {
+            AA aa = new AA();
+
+            Util.Print(aa, "Hello");
+            aa.Print("Hello");
+
+            Util.Sum(10);
+            10.Sum();
         }
     }
 }
