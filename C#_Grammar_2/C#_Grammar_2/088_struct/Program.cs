38a5c84d09b5199c05aea6ae14f196a054f3642c
jaehong park <woghd8503@naver.com>
2024-05-22 09:04:23 +0900

    088_struct

--- C#_Grammar_2/C#_Grammar_2/088_struct/Program.cs
+++ C#_Grammar_2/C#_Grammar_2/088_struct/Program.cs
@@ -4,12 +4,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*-----------------------------------------------------------------------------
+ * Name: _088_struct
+ * DESC: 구조체 기본
+-----------------------------------------------------------------------------*/
 namespace _088_struct
 {
+    struct AA
+    {
+        public int x;
+        public int y;
+
+        //public AA() //오류: 파라미터 없음
+        public AA(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("x:   {0},  y:   {1}", x, y);
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            AA aa = new AA(10, 20);
+            aa.x = 100;
+            aa.Print();
+
+            AA aaa;
+            aaa.x = 100;
+            aaa.y = 200;
+            aaa.Print();
+
+            AA copyAA = aa;
+            copyAA.x = 1000;
+            copyAA.x = 2000;
+            aa.Print();
+            copyAA.Print();
         }
     }
 }
