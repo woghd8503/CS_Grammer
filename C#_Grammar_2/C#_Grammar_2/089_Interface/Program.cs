9ce7976486ff3e73b21498bcbd5414aa77f35a4a
jaehong park <woghd8503@naver.com>
2024-05-23 18:33:37 +0900

    089_interface

---
+++ C#_Grammar_2/C#_Grammar_2/089_Interface/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*-----------------------------------------------------------------------------
+ * Name: _089_interface
+ * DESC: interface 기본
+-----------------------------------------------------------------------------*/
+
+namespace _089_interface
+{
+    interface IAA
+    {
+        public int a; //에러
+        private void IPrint() { } //에러..
+        public void IPrint(); //에러..
+        void IAAPrint();
+    }
+
+    interface IBB
+    {
+        void IBBPrint();
+    }
+
+
+}
+
