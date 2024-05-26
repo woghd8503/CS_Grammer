0201ce73d3c12724824bc2b406b1995fc4e8f81d
jaehong park <woghd8503@naver.com>
2024-05-26 09:03:11 +0900

    089_Interface

--- C#_Grammar_2/C#_Grammar_2/089_Interface/Program.cs
+++ C#_Grammar_2/C#_Grammar_2/089_Interface/Program.cs
@@ -13,9 +13,9 @@ namespace _089_interface
 {
     interface IAA
     {
-        public int a; //에러
-        private void IPrint() { } //에러..
-        public void IPrint(); //에러..
+        //public int a; //에러
+        //private void IPrint() { } //에러..
+        //public void IPrint(); //에러..
         void IAAPrint();
     }
 
@@ -24,6 +24,25 @@ namespace _089_interface
         void IBBPrint();
     }
 
+    class Super
+    {
+        protected int num;
+
+        public virtual void Print()
+        {
+            Console.WriteLine("===========================================");
+        }
+    }
+
+    class AA : IAA 
+    {
+        public void IAAPrint()
+        {
+            Console.WriteLine("class AA interface IAA에 IAAPrint() 재정의");
+        }
+    }
+
+    
 
 }
 
