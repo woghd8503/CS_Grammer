3838fac75673c25f765c02aebbba1089bb535b9c
jaehong park <woghd8503@naver.com>
2024-06-02 00:00:00 +0900

    091_Property01

---
+++ C#_Grammar_2/C#_Grammar_2/091_Property01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _091_Property01
+{
+    class AA
+    {
+        private int num;
+
+        public int NUM
+        {
+            get { return num; }
+            set { num = value; } //set 부분을 구현하지 않으면 readonly와 같은 읽기 전용
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AA aa = new AA();
+            aa.NUM = 100;
+
+            Console.WriteLine("aa.NUM: {0}", aa.NUM);
+        }
+    }
+}
