a293820b84796306c240966af65f129d8e9b8807
jaehong park <woghd8503@naver.com>
2024-06-23 23:59:51 +0900

    094_Check

---
+++ C#_Grammar_2/C#_Grammar_2/094_Check/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _094_Check
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
