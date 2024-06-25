efae5d18161778fb3ab46f13429ecca007c7674f
jaehong park <woghd8503@naver.com>
2024-06-25 09:28:36 +0900

    094_Check

--- C#_Grammar_2/C#_Grammar_2/094_Check/Program.cs
+++ C#_Grammar_2/C#_Grammar_2/094_Check/Program.cs
@@ -6,6 +6,18 @@ using System.Threading.Tasks;
 
 namespace _094_Check
 {
+    class CSaveNumber
+    {
+        private int number1 { get; set; }
+        private int number2 { get; set; }
+
+        public CSaveNumber()
+        {
+            number1 = 0;
+            number2 = 0;
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
