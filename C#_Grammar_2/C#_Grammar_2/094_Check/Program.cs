a14543735740a31db47a3f2979e3a7254cee8841
jaehong park <woghd8503@naver.com>
2024-07-03 08:27:38 +0900

    094_Check

--- C#_Grammar_2/C#_Grammar_2/094_Check/Program.cs
+++ C#_Grammar_2/C#_Grammar_2/094_Check/Program.cs
@@ -16,9 +16,22 @@ namespace _094_Check
             number1 = 0;
             number2 = 0;
         }
+
+        public void InputNumber(int count)
+        {
+            if(count == 0)
+            {
+                Console.Write("첫번째 수를 입력 해 주세요?");
+                number1 = int.Parse(Console.ReadLine());
+            }
+            else
+            {
+                Console.Write("두번째 수를 입력 해주세요?");
+            }
+        }
     }
 
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
