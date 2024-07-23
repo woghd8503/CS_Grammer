db886d3f6e5b591484bbcf6902d9d029622d6fc8
jaehong park <woghd8503@naver.com>
2024-07-23 09:43:10 +0900

    098_Hashtable

---
+++ C#_Grammar_2/C#_Grammar_2/098_Hashtable/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*-----------------------------------------------------------------------------
+ * Name: _098_Hashtable
+ * DESC: Hashtable 기본
+-----------------------------------------------------------------------------*/
+namespace _098_Hashtable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable hashTable = new Hashtable();
+            hashTable.Add("pos", 10);
+        }
+    }
+}
