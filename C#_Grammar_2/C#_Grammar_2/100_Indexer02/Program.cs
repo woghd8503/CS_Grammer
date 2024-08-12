fffcdc6fdc0ad44ec8769be48944b1e12fcf0154
jaehong park <woghd8503@naver.com>
2024-08-12 23:21:58 +0900

    100_indexer02

---
+++ C#_Grammar_2/C#_Grammar_2/100_Indexer02/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*-----------------------------------------------------------------------------
+ * Name: _100_Indexer02
+ * DESC: 
+-----------------------------------------------------------------------------*/
+namespace _100_Indexer02
+{
+    class AA
+    {
+        ArrayList arrayList = new ArrayList();
+
+        public string this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < arrayList.Count)
+                {
+                    return (string)arrayList[index];
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            set
+            {
+                if (index >= 0 && index < arrayList.Count)
+                {
+                    arrayList[index] = value;
+                }
+                else if (index == arrayList.Count)
+                {
+                    arrayList.Add(value);
+                }
+            }
+        }
+
+        public int count
+        {
+            get { return arrayList.Count; }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AA aa = new AA();
+            for(int i = 0; i < 10; i++)
+            {
+                aa[i] = string.Format("{0}", i);
+            }
+
+            aa[0] = "Hello";
+            aa[1] = "World";
+
+            for(int i = 0; i < aa.count; i++)
+            {
+                Console.WriteLine("aa: " + aa[i]);
+            }
+        }
+    }
+}
