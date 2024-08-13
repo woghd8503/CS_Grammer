8eb692028ad613509093137a129b722880a1a919
jaehong park <woghd8503@naver.com>
2024-08-13 09:21:49 +0900

    101_Check

---
+++ C#_Grammar_2/C#_Grammar_2/101_Check/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _101_Check
+{
+    class CStudent
+    {
+        private int id;
+        private int kor;
+        private int math;
+        private int eng;
+
+        public int ID { get { return id; } }
+        public int KOR { get { return kor; } }
+        public int MATH { get { return math; } }
+        public int ENG { get { return eng; } }
+
+        public CStudent()
+        {
+            this.id = 0;
+            this.kor = 0;
+            this.math = 0;
+            this.eng = 0;
+        }
+
+        public void InputID()
+        {
+            Console.Write("학생 ID를 입력하세요?  ");
+            this.id = int.Parse(Console.ReadLine());
+        }
+
+        public void InputKor()
+        {
+
+        }
+    }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
