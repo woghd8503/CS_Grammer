030d820c34ce0e3bb78122332095fe7fea4661ff
jaehong park <woghd8503@naver.com>
2024-09-02 00:00:00 +0900

    102_Check

--- C#_Grammar_2/C#_Grammar_2/102_Check/Program.cs
+++ C#_Grammar_2/C#_Grammar_2/102_Check/Program.cs
@@ -17,7 +17,28 @@ namespace _102_Check
             number2 = 0;
         }
 
+        public void InputNumber(int count)
+        {
+            if(count == 0)
+            {
+                Console.Write("첫번째 수를 입력 해 주세요?");
+                number1 = int.Parse(Console.ReadLine());
+            }
+            else
+            {
+                Console.Write("두번째 수를 입력 해 주세요?");
+                number2 = int.Parse(Console.ReadLine());
+            }
+        }
+
+        public void PrintResult()
+        {
+            Console.Write("{0} + {1} = {2}", number1, number2, (number1 + number2));
+            Console.WriteLine();
+        }
     }
+
+
     class Program
     {
         static void Main(string[] args)
