a9adcaa68f853018583d2e9ec9ad5e87afc34df6
jaehong park <woghd8503@naver.com>
2024-09-04 21:28:40 +0900

    102_Check

---
+++ C#_Grammar_2/C#_Grammar_2/102_Check/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _102_Check
+{
+    class CSaveNumber
+    {
+        private int number1 { get; set; }
+        private int number2 { get; set; }
+
+        public CSaveNumber() 
+        {
+            number1 = 0;
+            number2 = 0;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
