e0553826663e4886a1e3b05b2a6dce766870f541
jaehong park <woghd8503@naver.com>
2024-10-10 09:54:24 +0900

    108_Queue

---
+++ C#_Grammar_2/C#_Grammar_2/108_Queue/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*-----------------------------------------------------------------------------
+ * Name: _108_Queue
+ * DESC: Queue<T> 기초
+-----------------------------------------------------------------------------*/
+namespace _108_Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queueAA = new Queue<int>();
+            queueAA.Enqueue(1);
+            queueAA.Enqueue(2);
+
+            for(int i = 0; i < 10; i++)
+            {
+                queueAA.Enqueue(i);
+            }
+        }
+    }
+}
---
+++ C#_Grammar_2/C#_Grammar_2/108_Queue/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("108_Queue")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("108_Queue")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c29ccfa0-9f26-483e-89a1-ca300527debf")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
