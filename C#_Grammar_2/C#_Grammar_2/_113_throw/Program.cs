02bc6059fd207364d83cdf992a9adc467366632d
jaehong park <woghd8503@naver.com>
2024-10-24 23:31:11 +0900

    113_throw

---
+++ C#_Grammar_2/C#_Grammar_2/_113_throw/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*-----------------------------------------------------------------------------
+ * Name: _113_throw
+ * DESC: throw 기초
+ * 조건 연산자에서 사용된다.
+-----------------------------------------------------------------------------*/
+namespace _113_throw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
