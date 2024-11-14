9476bd802392ea85fae4ba51eacb2648703c4980
jaehong park <woghd8503@naver.com>
2024-11-14 09:14:43 +0900

    113_throw

---
+++ C#_Grammar_2/C#_Grammar_2/113_throw/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*-----------------------------------------------------------------------------
+ * Name: _113_throw
+ * DESC: throw 기초
+ * 조건 연산자에서 사용된다.
+-----------------------------------------------------------------------------*/
+namespace _113_throw
+{
+    class Program
+    {
+        static void ThrowFunc(int data)
+        {
+            if(data > 0)
+            {
+                Console.WriteLine("ThrowFunc data: " + data);
+            }
+            else
+            {
+                throw new Exception("data에 0이 입력되었습니다.");
+            }
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ C#_Grammar_2/C#_Grammar_2/113_throw/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("113_throw")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("113_throw")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("95cccd7a-bd90-45f5-b641-64a6fc19d285")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
