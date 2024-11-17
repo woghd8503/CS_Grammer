e0eefe65b0a16294d2116a34bc3cfaf7ac0d842e
jaehong park <woghd8503@naver.com>
2024-11-17 10:10:00 +0900

    git commit

--- C#_Gramma_1/003_HellowWorld3/obj/Release/net6.0/003_HelloWorld3.AssemblyInfo.cs
+++ C#_Gramma_1/003_HellowWorld3/obj/Release/net6.0/003_HelloWorld3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("003_HelloWorld3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ff54c2f702630c3d954c294146c70411484fa18")]
 [assembly: System.Reflection.AssemblyProductAttribute("003_HelloWorld3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("003_HelloWorld3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
