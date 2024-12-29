659aea25b9170a203c3bafedaa8d557bbf91caae
jaehong park <woghd8503@naver.com>
2024-12-30 08:52:49 +0900

    115_CustomException

---
+++ C#_Grammar_2/C#_Grammar_2/115_CustomException/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*-----------------------------------------------------------------------------
+ * Name: _115_CustomException
+ * DESC: CustomException 기초, when 키워드, StackTrace 키워드
+-----------------------------------------------------------------------------*/
+namespace _115_CustomException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
