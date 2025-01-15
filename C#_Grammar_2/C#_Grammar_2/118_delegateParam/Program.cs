b1195ccf9ab3deb8dbc1ae839348308995272508
jaehong park <woghd8503@naver.com>
2025-01-15 23:04:54 +0900

    118_delegateParam

---
+++ C#_Grammar_2/C#_Grammar_2/118_delegateParam/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*-----------------------------------------------------------------------------
+ * Name: _118_delegateParam
+ * DESC: delegate 파라미터
+-----------------------------------------------------------------------------*/
+namespace _118_delegateParam
+{
+    delegate void delegateFunc();
+
+    class MessageProcess
+    {
+        delegateFunc CallOkFunc;
+        delegateFunc CallCancelFunc;
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
